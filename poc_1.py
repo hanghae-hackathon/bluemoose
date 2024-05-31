08f8973e0b7a265b050e3ceafc79f8a61e289294
Kyuyoung11 <kyuyoung.lee@bankwareglobal.com>
2024-06-01 02:16:20 +0900

    (feat) 면책조항 조회 LLM

--- poc_1.py
+++ poc_1.py
@@ -1,21 +1,27 @@
 # pip install PyPDF2
 import PyPDF2
 from openai import OpenAI
+import os
 
 # LLM에 질의
 client = OpenAI(
     # This is the default and can be omitted
-    # api_key=os.environ.get("OPENAI_API_KEY"),
+    api_key= os.environ["OPENAI_API_KEY"]
 )
 
 # PDF 파일 열기
 pdf_file = open("플러스메디컬단체보험_특별약관.pdf", 'rb')
 pdf_reader = PyPDF2.PdfReader(pdf_file)
 
+
 for page in range(len(pdf_reader.pages)):
     text = pdf_reader.pages[page].extract_text()
-    prompt = f"다음 텍스트에서 보험 계약의 면책조항을 찾아주세요:\n\n{text}"
+# text = pdf_reader.pages[2].extract_text()
 
+    prompt = f"""{text}\n
+        위 텍스트는 보험 계약의 약관입니다. 면책조항이 있으면 Y, 없으면 N으로 답해주세요.\n
+        있을 경우에는 몇 조의 몇 항인지만 적어주세요."""
+    
     response = client.chat.completions.create(
         messages=[
             {
@@ -23,19 +29,12 @@ for page in range(len(pdf_reader.pages)):
                 "content": prompt
             }
         ],
-        model="gpt-4o",
+        model="gpt-4",
+        temperature=0.2
     )
 
     # 응답 출력
-    print(f"@.@ page: {page} - {response.choices[0].message.content}")
+    print(f"Page : {page} - {response.choices[0].message.content}")
 
-# # response = client.chat.completions.create(
-# #     engine="text-davinci-003",
-# #     prompt=prompt,
-# #     max_tokens=1024,
-# #     n=1,
-# #     stop=None,
-# #     temperature=0.5,
-# # )
 
 pdf_file.close()
